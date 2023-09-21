3d4b04e3c67f4ad31352a2f30160c3c976422f86
Ann <ann.mitrakhovich@gmail.com>
2023-09-21 13:30:49 +0300

    start auto home task

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-//
+//Напишите функцию printPrimeNums, которая выведет на экран все простые числа в промежутке от 1 до 1000,
+// каждое на новой строке.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
