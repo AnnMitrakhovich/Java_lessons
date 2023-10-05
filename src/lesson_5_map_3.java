0166df781d7649b77baac94b32067389407c55ce
Ann <ann.mitrakhovich@gmail.com>
2023-10-05 13:57:06 +0300

    correct placement of brackets done

--- src/lesson_5_map_3.java
+++ src/lesson_5_map_3.java
@@ -6,63 +6,40 @@
         Пример 5: <{a}+{(d*3)}> - истина
         Пример 6: {a+]}{(d*3)} - ложь */
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 
 public class lesson_5_map_3 {
     public static void main(String[] args) {
-        bracks("<{a}+{(d*3)}>");
+
+        System.out.println(checkBracks("<{a+{(d*3)}>"));
 
     }
 
-
-    public static boolean bracks (String str) {
-        //char[] openBracks = {'(','[','{','<'};
-        //char[] closeBracks = {')',']','}','>'};
+    public static boolean checkBracks(String str) {
         char[] strArr = str.toCharArray();
-        Map<Character,Integer> bracks = new HashMap<>();
-        int count = 0;
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
+        Map<Character, Character> mapping = new HashMap<>();
+        Deque<Character> res = new ArrayDeque<>();
+        mapping.put('{', '}');
+        mapping.put('[', ']');
+        mapping.put('(', ')');
+        mapping.put('<', '>');
+        for (char elements : strArr) {
+            if (mapping.containsKey(elements)) {
+                res.add(elements);
+                continue;
 
-        bracks.put('(', count);
-        bracks.put('[',count1);
-        bracks.put('{',count2);
-        bracks.put('<',count3);
-        for (char elements: strArr) {
-            if (bracks.get(elements) != null) {
-                bracks.put(elements,count++);
+            } else if (mapping.containsValue(elements)) {
+                if (res.isEmpty()) {
+                    return false;
+                } else {
+                    res.removeLast();
+                }
             }
-
         }
-        System.out.println(bracks);
-        return true;
+        return res.isEmpty();
     }
 }
-//  public static boolean equals(String str) {
-//        char[] result = str.toCharArray();
-//        int count = 1;
-////        Map<Character, Integer> bracks = new HashMap<>();
-//        Map<Character, Character> mapping = new HashMap<>();
-//        Deque<Character> res = new ArrayDeque<>();
-//        mapping.put('{', '}');
-//        mapping.put('[', ']');
-//        mapping.put('(', ')');
-//        mapping.put('<', '>');
-//
-//        for(char element : result){
-//
-//            if(mapping.containsKey(element)){
-//                res.push(element);
-//            } else if (mapping.containsValue(element) ){
-//                if(res.isEmpty()){
-//                    return false;
-//                }
-//                res.remove();
-//            }
-//
-//        }
-//
-//    }
-//}
+
