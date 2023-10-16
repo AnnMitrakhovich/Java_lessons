0244ec5ac03d795505ab2edcb279872d162fa311
Ann <ann.mitrakhovich@gmail.com>
2023-10-16 23:49:56 +0300

    void bubble sort done

--- src/lesson_2_BubbleSort.java
+++ src/lesson_2_BubbleSort.java
@@ -6,6 +6,13 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+/*Реализуйте алгоритм сортировки пузырьком числового массива, 
+результат после каждой итерации запишите в лог-файл.
+Запишите свой код в методе sort класса BubbleSort. Метод sort принимает на вход один параметр:
+int[] arr - числовой массив
+После каждой итерации, ваш код должен делать запись в лог-файл 'log.txt' 
+в формате год-месяц-день час:минуты {массив на данной итерации}.*/
+
 public class lesson_2_BubbleSort {
     public static void main(String[] args) {
         int[] arr = {};
@@ -22,14 +29,24 @@ public class lesson_2_BubbleSort {
         }
     }
 
+    public static class BubbleSort {
+        private static File log;
+        private static FileWriter fileWriter;
 
-        public static class BubbleSort {
-            private static File log;
-            private static FileWriter fileWriter;
+        public static void sort(int[] mas) {
+            int temp = mas[0];
+            for (int i = mas.length - 1; i >= 1; i--) {
+                for (int j = 0; j < i; j++) {
+                    if (mas[j] > mas[j + 1]) {
+                        temp = mas[j];
+                        mas[j] = mas[j + 1];
+                        mas[j + 1] = temp;
+                    }
 
-            public static void sort(int[] mas) {
-
+                }
             }
+
         }
     }
+}
 
